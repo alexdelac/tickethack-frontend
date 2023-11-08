15fb0f8e1d048a2ec706cddad301f7932f482ae0
Marie <mme.sabre@outlook.fr>
2023-11-08 14:16:43 +0100

    remove sur bouton delete cart.js

--- cart.js
+++ cart.js
@@ -1,3 +1,20 @@
+function deleteCart(){
+    for (let i=0; i<document.querySelectorAll('.deleteTrip').length; i++){
+        document.querySelectorAll('.deleteTrip')[i].addEventListener('click', function(){
+            console.log(this)
+            fetch(`http://localhost:3000/carts/${this.value}`,{
+                method: 'DELETE'})
+                .then(response =>response.json())
+                .then()
+                console.log(this.parentNode)
+                this.parentNode.remove()
+                
+        })
+    }
+}
+
+
+
 fetch('http://localhost:3000/carts')
 .then(response => response.json())
 .then(data => {
@@ -16,18 +33,24 @@ if(newCart.length != 0){
         <div class="trip">
             <p>${newCart[i].trip.departure}>${newCart[i].trip.arrival}</p>
             <p>${date.getHours()}h${date.getMinutes()}</p>
-            <p>${newCart[i].trip.price}€<p/>
-            <button type="submit" class="addTrip" value="${newCart[i].trip._id}">X</button>
+            <p>${newCart[i].trip.price}€</p>
+            <button type="submit" class="deleteTrip" value="${newCart[i]._id}">X</button>
         </div>
         `
     
     }
+
     document.querySelector('#cart-container').innerHTML +=
     `
     <div>
         <p>Total:${total}</p>
-        <button type="submit" class="addTrip">Purchase</button>
+        <button type="submit" class="purchaseTrips">Purchase</button>
     </div>
     `
+    deleteCart()
 }
 }) 
+
+
+
+
